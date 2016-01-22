e1e64cf6e80dc830f403f258e51592c6b404fffc
gree-gorey <ignatyeff.g@yandex.ru>
2016-01-22 22:25:48 +0300

    add normalization of vectors

--- creator.py
+++ creator.py
@@ -3,6 +3,7 @@
 import time
 import codecs
 import pickle
+import copy
 
 __author__ = 'Gree-gorey'
 
@@ -12,53 +13,59 @@ t1 = time.time()
 class Store:
     def __init__(self):
         self.words = []
+        self.min = []
+        self.max = []
 
 
 class Noun:
     def __init__(self):
         self.name = u''
-        self.stability = None
-        self.complexity = None
-        self.familiarity = None
-        self.age = None
-        self.representability = None
-        self.similarity = None
-        self.frequency = None
-        self.syl_length = None
-        self.ph_length = None
+        self.features = []
 
     def __gt__(self, other):
-        return self.age > other.age
+        return self.features[1] > other.features[1]
 
     def __lt__(self, other):
-        return self.age < other.age
+        return self.features[1] < other.features[1]
 
     def __eq__(self, other):
-        return self.age == other.age
+        return self.features[1] == other.features[1]
 
 
 with codecs.open(u'/home/gree-gorey/stimdb/nouns.csv', u'r', u'utf-8') as f:
     newStore = Store()
+    j = 0
     for line in f:
         line = line.rstrip(u'\n').split(u'\t')
         newStore.words.append(Noun())
         newStore.words[-1].name = line[0]
-        newStore.words[-1].stability = line[1]
-        newStore.words[-1].complexity = line[2]
-        newStore.words[-1].familiarity = line[3]
-        newStore.words[-1].age = float(line[4])
-        newStore.words[-1].representability = line[5]
-        newStore.words[-1].similarity = line[6]
-        newStore.words[-1].frequency = line[7]
-        newStore.words[-1].syl_length = line[8]
-        newStore.words[-1].ph_length = line[9]
+        newStore.words[-1].features = [float(x) for x in line[1::]]
+        if j == 0:
+            newStore.min = copy.deepcopy(newStore.words[-1].features)
+            newStore.max = copy.deepcopy(newStore.words[-1].features)
+        for i in xrange(len(newStore.words[-1].features)):
+            if newStore.words[-1].features[i] < newStore.min[i]:
+                newStore.min[i] = newStore.words[-1].features[i]
+            if newStore.words[-1].features[i] > newStore.max[i]:
+                newStore.max[i] = newStore.words[-1].features[i]
+        j += 1
 
 # for noun in sorted(newStore.words):
 #     print noun.name, noun.age
 
-w = codecs.open(u'/home/gree-gorey/stimdb/nouns.p', u'w', u'utf-8')
-pickle.dump(newStore, w)
-w.close()
+print newStore.min, newStore.max
+
+for noun in newStore.words:
+    for i in xrange(len(noun.features)):
+        noun.features[i] = (noun.features[i] - newStore.min[i]) / (newStore.max[i] - newStore.min[i])
+
+for noun in newStore.words:
+    if noun.features[4] == 0:
+        print noun.features[4]
+
+# w = codecs.open(u'/home/gree-gorey/stimdb/nouns.p', u'w', u'utf-8')
+# pickle.dump(newStore, w)
+# w.close()
 
 # pickle.load(f)
 
