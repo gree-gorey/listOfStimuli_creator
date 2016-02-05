bf23699bdf31b804562d88a6435ad1f8ba7c28f5
gree-gorey <ignatyeff.g@yandex.ru>
2016-02-05 21:08:46 +0300

    add human classes and methods

--- creator.py
+++ creator.py
@@ -1,11 +1,7 @@
 # -*- coding:utf-8 -*-
 
-import random
 import time
-import codecs
 import pickle
-import zipfile
-import copy
 from scipy import stats
 
 __author__ = 'Gree-gorey'
@@ -13,247 +9,51 @@ __author__ = 'Gree-gorey'
 t1 = time.time()
 
 
-class Store:
-    def __init__(self):
-        self.words = []
-        self.min = []
-        self.max = []
-        self.high = []
-        self.low = []
-        self.high_output = []
-        self.low_output = []
-        self.minimum = None
+with open(u'/home/gree-gorey/stimdb/store.p', u'r') as f:
+    newStore = pickle.load(f)
 
-
-class Noun:
-    def __init__(self):
-        self.name = u''
-        self.features = []
-        self.normalized_features = []
-        self.same = 0
-        self.diff = 0
-        self.distance = 1
-
-    def __gt__(self, other):
-        return self.diff > other.diff
-
-    def __lt__(self, other):
-        return self.diff < other.diff
-
-    def __eq__(self, other):
-        return self.diff == other.diff
-
-
-def create_zip(list_1, list_2):
-    with codecs.open(u'./data/list_1.csv', u'w', u'utf-8') as w:
-        w.write(list_1)
-
-    with codecs.open(u'./data/list_2.csv', u'w', u'utf-8') as w:
-        w.write(list_2)
-
-    Z = zipfile.ZipFile(u'output.zip', u'w')
-
-    Z.write(u'./data/list_1.csv')
-    Z.write(u'./data/list_2.csv')
-
-
-def mean(arr):
-    return sum(arr)/len(arr) if len(arr) > 0 else None
-
-
-def compensate(higher, lower, i):
-    lowest_from_rest = 1
-    hhh = 0
-    for j in xrange(len(higher)):
-        if higher[j].normalized_features[i] < lowest_from_rest:
-            lowest_from_rest = higher[j].normalized_features[i]
-            hhh = j
-
-    highest_from_rest = 0
-    lll = 0
-    for j in xrange(len(lower)):
-        if lower[j].normalized_features[i] > highest_from_rest:
-            highest_from_rest = lower[j].normalized_features[i]
-            lll = j
-
-    return hhh, lll
-
-
-newStore = Store()
-
-with codecs.open(u'/home/gree-gorey/stimdb/nouns.csv', u'r', u'utf-8') as f:
-    j = 0
-    for line in f:
-        line = line.rstrip(u'\n').split(u'\t')
-        newStore.high.append(Noun())
-        newStore.high[-1].name = line[0]
-        newStore.high[-1].features = [float(x) for x in line[1::]]
-        if j == 0:
-            newStore.min = copy.deepcopy(newStore.high[-1].features)
-            newStore.max = copy.deepcopy(newStore.high[-1].features)
-        for i in xrange(len(newStore.high[-1].features)):
-            if newStore.high[-1].features[i] < newStore.min[i]:
-                newStore.min[i] = newStore.high[-1].features[i]
-            if newStore.high[-1].features[i] > newStore.max[i]:
-                newStore.max[i] = newStore.high[-1].features[i]
-        j += 1
-
-with codecs.open(u'/home/gree-gorey/stimdb/verbs.csv', u'r', u'utf-8') as f:
-    j = 0
-    for line in f:
-        line = line.rstrip(u'\n').split(u'\t')
-        newStore.low.append(Noun())
-        newStore.low[-1].name = line[0]
-        newStore.low[-1].features = [float(x) for x in line[1::]]
-        if j == 0:
-            newStore.min = copy.deepcopy(newStore.low[-1].features)
-            newStore.max = copy.deepcopy(newStore.low[-1].features)
-        for i in xrange(len(newStore.low[-1].features)):
-            if newStore.low[-1].features[i] < newStore.min[i]:
-                newStore.min[i] = newStore.low[-1].features[i]
-            if newStore.low[-1].features[i] > newStore.max[i]:
-                newStore.max[i] = newStore.low[-1].features[i]
-        j += 1
-
-
-# parameters = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-parameters = [1, 2, 8, 9]
+# same = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+same = [1, 2, 8, 9]
 # different = 7
-N = len(parameters)
+length = 50
 
+newStore.setup_parameters(same, length)
 
-for word in newStore.high:
-    word.normalized_features = copy.deepcopy(word.features)
-    for i in xrange(len(word.features)):
-        word.normalized_features[i] = (word.features[i] - newStore.min[i]) / (newStore.max[i] - newStore.min[i])
-    word.same = [word.normalized_features[i] for i in parameters]
-    # noun.diff = noun.normalized_features[different]
+while newStore.sharp():
+    newStore.add_first()
 
-for word in newStore.low:
-    word.normalized_features = copy.deepcopy(word.features)
-    for i in xrange(len(word.features)):
-        word.normalized_features[i] = (word.features[i] - newStore.min[i]) / (newStore.max[i] - newStore.min[i])
-    word.same = [word.normalized_features[i] for i in parameters]
-
-# newStore.backup_low = copy.deepcopy(newStore.low)
-# newStore.backup_high = copy.deepcopy(newStore.high)
-
-M = 400
-
-while len(newStore.high_output) != M:
-    newStore.low += newStore.low_output
-    newStore.high += newStore.high_output
-
-    newStore.low_output = []
-    newStore.high_output = []
-
-    index = random.randint(0, len(newStore.low)-1)
-
-    newStore.low_output.append(newStore.low[index])
-    del newStore.low[index]
-
-    index = random.randint(0, len(newStore.high)-1)
-
-    newStore.high_output.append(newStore.high[index])
-    del newStore.high[index]
-
-    allow = True
     end = False
     p_value_diff = 0
 
-    while allow and len(newStore.high_output) < M:
-        distance_for_low = []
-        for i in xrange(N):
-            distance_for_low.append(mean([word.same[i] for word in newStore.high_output]))
+    while newStore.allow and newStore.less():
+        newStore.add_closest()
+        if len(newStore.first_list_output) > 15:
+            newStore.test_and_fix()
 
-        minimum = N
-        index = 0
-        for i in xrange(len(newStore.low)):
-            from_distance = sum([abs(newStore.low[i].same[j] - distance_for_low[j]) for j in xrange(N)])
-            if from_distance < minimum:
-                minimum = from_distance
-                index = i
-
-        newStore.low_output.append(newStore.low[index])
-        del newStore.low[index]
-
-        distance_for_high = []
-        for i in xrange(N):
-            distance_for_high.append(mean([word.same[i] for word in newStore.low_output]))
-
-        minimum = N
-        index = 0
-        for i in xrange(len(newStore.high)):
-            from_distance = sum([abs(newStore.high[i].same[j] - distance_for_high[j]) for j in xrange(N)])
-            if from_distance < minimum:
-                minimum = from_distance
-                index = i
-
-        newStore.high_output.append(newStore.high[index])
-        del newStore.high[index]
-
-        if len(newStore.high_output) > 15:
-            # p_value_diff = stats.ttest_ind([noun.normalized_features[different] for noun in newStore.high_output],
-            #                                [noun.normalized_features[different] for noun in newStore.low_output])[1]
-
-            for i in parameters:
-                p_value_same = stats.ttest_ind([word.normalized_features[i] for word in newStore.high_output],
-                                               [word.normalized_features[i] for word in newStore.low_output])[1]
-
-                if p_value_same < 0.1:
-                    high_mean = mean([word.normalized_features[i] for word in newStore.high_output])
-                    low_mean = mean([word.normalized_features[i] for word in newStore.low_output])
-
-                    if len(newStore.high_output) == M:
-                        newStore.high.append(newStore.high_output.pop(random.randint(0, len(newStore.high_output)-1)))
-                        newStore.low.append(newStore.low_output.pop(random.randint(0, len(newStore.low_output)-1)))
-
-                    if high_mean > low_mean:
-                        hhh, lll = compensate(newStore.high, newStore.low, i)
-                        newStore.high_output.append(newStore.high[hhh])
-                        newStore.low_output.append(newStore.low[lll])
-                    else:
-                        hhh, lll = compensate(newStore.low, newStore.high, i)
-                        newStore.high_output.append(newStore.high[lll])
-                        newStore.low_output.append(newStore.low[hhh])
-
-                    for k in parameters:
-                        p_value_same = stats.ttest_ind([noun.normalized_features[k] for noun in newStore.high_output],
-                                                       [noun.normalized_features[k] for noun in newStore.low_output])[1]
-
-                        if p_value_same < 0.05:
-                            end = True
-
-        if end or p_value_diff > 0.05:
-            allow = False
-
-print len(newStore.high_output), len(newStore.low_output)
+print len(newStore.first_list_output), len(newStore.second_list_output)
 
 print '\n######################################\n'
 
+# p_value_diff = stats.ttest_ind([noun.normalized_features[different] for noun in newStore.first_list_output],
+# [noun.normalized_features[different] for noun in newStore.second_list_output])[1]
+
 # for i in xrange(6):
-#     print newStore.low_output[i].name
+#     print newStore.second_list_output[i].name
 
-# w = codecs.open(u'/home/gree-gorey/stimdb/nouns.p', u'w', u'utf-8')
-# pickle.dump(newStore, w)
-# w.close()
-
-# pickle.load(f)
-
-for i in parameters:
-    p_value_same = stats.ttest_ind([noun.normalized_features[i] for noun in newStore.high_output],
-                                       [noun.normalized_features[i] for noun in newStore.low_output])[1]
+for i in newStore.same:
+    p_value_same = stats.ttest_ind([noun.normalized_features[i] for noun in newStore.first_list_output],
+                                       [noun.normalized_features[i] for noun in newStore.second_list_output])[1]
 
     print p_value_same
 
 print '\n######################################\n'
 
-# with codecs.open(u'/home/gree-gorey/stimdb/high.csv', u'w', u'utf-8') as w:
-#     for word in newStore.high_output:
+# with codecs.open(u'/home/gree-gorey/stimdb/first_list.csv', u'w', u'utf-8') as w:
+#     for word in newStore.first_list_output:
 #         w.write(word.name + u'\t' + u'\t'.join([str(f) for f in word.features]) + u'\n')
 #
-# with codecs.open(u'/home/gree-gorey/stimdb/low.csv', u'w', u'utf-8') as w:
-#     for word in newStore.low_output:
+# with codecs.open(u'/home/gree-gorey/stimdb/second_list.csv', u'w', u'utf-8') as w:
+#     for word in newStore.second_list_output:
 #         w.write(word.name + u'\t' + u'\t'.join([str(f) for f in word.features]) + u'\n')
 
 t2 = time.time()
---
+++ structures.py
@@ -0,0 +1,209 @@
+# -*- coding:utf-8 -*-
+
+import random
+import codecs
+import zipfile
+from scipy import stats
+
+__author__ = 'Gree-gorey'
+
+
+class Store:
+    def __init__(self):
+        self.min = []
+        self.max = []
+        self.first_list = []
+        self.second_list = []
+        self.nouns = []
+        self.verbs = []
+        self.first_list_output = []
+        self.second_list_output = []
+        self.minimum = None
+        self.arr = []
+        self.nouns_min = []
+        self.nouns_max = []
+        self.verbs_min = []
+        self.verbs_max = []
+        self.length = 0
+        self.number_of_same = 0
+        self.allow = True
+        self.same = []
+
+    def read_words(self, f, pos):
+        j = 0
+        for line in f:
+            line = line.rstrip(u'\n').split(u'\t')
+            self.arr.append(Word())
+            self.arr[-1].name = line[0]
+            self.arr[-1].features = [float(x) for x in line[1::]]
+            if j == 0:
+                self.min += self.arr[-1].features
+                self.max += self.arr[-1].features
+            for i in xrange(len(self.arr[-1].features)):
+                if self.arr[-1].features[i] < self.min[i]:
+                    self.min[i] = self.arr[-1].features[i]
+                if self.arr[-1].features[i] > self.max[i]:
+                    self.max[i] = self.arr[-1].features[i]
+            j += 1
+        if pos == u'noun':
+            self.nouns += self.arr
+            self.arr = []
+            self.nouns_min = self.min
+            self.nouns_max = self.max
+        elif pos == u'verb':
+            self.verbs += self.arr
+            self.arr = []
+            self.verbs_min = self.min
+            self.verbs_max = self.max
+
+    def normalize(self):
+        for word in self.nouns:
+            word.normalized_features += word.features
+            for i in xrange(len(word.features)):
+                word.normalized_features[i] = (word.features[i] - self.nouns_min[i]) / (self.nouns_max[i] - self.nouns_min[i])
+        for word in self.verbs:
+            word.normalized_features += word.features
+            for i in xrange(len(word.features)):
+                word.normalized_features[i] = (word.features[i] - self.verbs_min[i]) / (self.verbs_max[i] - self.verbs_min[i])
+
+    def create_zip(self):
+        with codecs.open(u'./data/list_1.csv', u'w', u'utf-8') as w:
+            w.write(self.first_list_output)
+
+        with codecs.open(u'./data/list_2.csv', u'w', u'utf-8') as w:
+            w.write(self.second_list_output)
+
+        z = zipfile.ZipFile(u'output.zip', u'w')
+        z.write(u'./data/list_1.csv')
+        z.write(u'./data/list_2.csv')
+
+    def test_and_fix(self):
+        for i in self.same:
+            p_value_same = stats.ttest_ind([word.normalized_features[i] for word in self.first_list_output],
+                                           [word.normalized_features[i] for word in self.second_list_output])[1]
+
+            if p_value_same < 0.1:
+                first_list_mean = mean([word.normalized_features[i] for word in self.first_list_output])
+                second_list_mean = mean([word.normalized_features[i] for word in self.second_list_output])
+
+                if self.sharp():
+                    self.first_list.append(self.first_list_output.pop(random.randint(0, len(self.first_list_output)-1)))
+                    self.second_list.append(self.second_list_output.pop(random.randint(0, len(self.second_list_output)-1)))
+
+                if first_list_mean > second_list_mean:
+                    hhh, lll = compensate(self.first_list, self.second_list, i)
+                    self.first_list_output.append(self.first_list[hhh])
+                    self.second_list_output.append(self.second_list[lll])
+                else:
+                    hhh, lll = compensate(self.second_list, self.first_list, i)
+                    self.first_list_output.append(self.first_list[lll])
+                    self.second_list_output.append(self.second_list[hhh])
+
+                for k in self.same:
+                    p_value_same = stats.ttest_ind([noun.normalized_features[k] for noun in self.first_list_output],
+                                                   [noun.normalized_features[k] for noun in self.second_list_output])[1]
+
+                    if p_value_same < 0.05:
+                        self.allow = False
+
+    def setup_parameters(self, same, length, different=None):
+        self.same = same
+        self.number_of_same = len(same)
+        self.length = length
+        if not different:
+            self.first_list += self.nouns
+            self.second_list += self.verbs
+        for word in self.first_list:
+            word.same = [word.normalized_features[i] for i in same]
+            # noun.diff = noun.normalized_features[different]
+        for word in self.second_list:
+            word.same = [word.normalized_features[i] for i in same]
+
+    def add_first(self):
+        self.first_list += self.first_list_output
+        self.second_list += self.second_list_output
+
+        self.first_list_output = []
+        self.second_list_output = []
+
+        index = random.randint(0, len(self.first_list)-1)
+        self.first_list_output.append(self.first_list[index])
+        del self.first_list[index]
+
+        index = random.randint(0, len(self.second_list)-1)
+        self.second_list_output.append(self.second_list[index])
+        del self.second_list[index]
+
+    def add_closest(self):
+        distance_for_first_list = []
+        for i in xrange(self.number_of_same):
+            distance_for_first_list.append(mean([word.same[i] for word in self.second_list_output]))
+        minimum = self.number_of_same
+        index = 0
+        for i in xrange(len(self.first_list)):
+            from_distance = sum([abs(self.first_list[i].same[j] - distance_for_first_list[j]) for j in xrange(self.number_of_same)])
+            if from_distance < minimum:
+                minimum = from_distance
+                index = i
+        self.first_list_output.append(self.first_list[index])
+        del self.first_list[index]
+
+        distance_for_second_list = []
+        for i in xrange(self.number_of_same):
+            distance_for_second_list.append(mean([word.same[i] for word in self.first_list_output]))
+        minimum = self.number_of_same
+        index = 0
+        for i in xrange(len(self.second_list)):
+            from_distance = sum([abs(self.second_list[i].same[j] - distance_for_second_list[j]) for j in xrange(self.number_of_same)])
+            if from_distance < minimum:
+                minimum = from_distance
+                index = i
+        self.second_list_output.append(self.second_list[index])
+        del self.second_list[index]
+
+    def sharp(self):
+        return len(self.first_list_output) != self.length
+
+    def less(self):
+        return len(self.first_list_output) < self.length
+
+
+class Word:
+    def __init__(self):
+        self.name = u''
+        self.features = []
+        self.normalized_features = []
+        self.same = 0
+        self.diff = 0
+        self.distance = 1
+
+    def __gt__(self, other):
+        return self.diff > other.diff
+
+    def __lt__(self, other):
+        return self.diff < other.diff
+
+    def __eq__(self, other):
+        return self.diff == other.diff
+
+
+def mean(arr):
+    return sum(arr)/len(arr) if len(arr) > 0 else None
+
+
+def compensate(higher, lower, i):
+    lowest_from_rest = 1
+    first_list_index = 0
+    for j in xrange(len(higher)):
+        if higher[j].normalized_features[i] < lowest_from_rest:
+            lowest_from_rest = higher[j].normalized_features[i]
+            first_list_index = j
+
+    highest_from_rest = 0
+    second_list_index = 0
+    for j in xrange(len(lower)):
+        if lower[j].normalized_features[i] > highest_from_rest:
+            highest_from_rest = lower[j].normalized_features[i]
+            second_list_index = j
+
+    return first_list_index, second_list_index
