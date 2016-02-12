c5a62fb40c6daf6843543fc181a756159b229b22
gree-gorey <ignatyeff.g@yandex.ru>
2016-02-12 13:16:44 +0300

    add start window

--- LoS_creator.py
+++ LoS_creator.py
@@ -7,9 +7,82 @@ from PyQt4.Qt import *
 __author__ = 'Gree-gorey'
 
 
-class Window(QWidget):
+class SetDiffer(QWidget):
     def __init__(self):
-        super(Window, self).__init__()
+        super(SetDiffer, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.center()
+        self.setWindowTitle(u'LoS creator 0.1')
+
+        # создаем главный грид
+        main_layout = QGridLayout()
+
+        def start():
+            TwoListsWindow()
+            # print arguments_list1.currentIndex()
+            # print cb.checkState(), button_group.checkedId()
+
+        def exit_f():
+            self.close()
+
+        def about():
+            w = QWidget()
+            QMessageBox.about(w, u'About', u'LoS creator version 0.1\n'
+                                           u'author: gree-gorey\n'
+                                           u'repository: https://github.com/gree-gorey/listOfStimuli_creator')
+            self.show()
+
+        # создаем поле кнопок
+        groupBox = QGroupBox()
+        hbox = QHBoxLayout()
+
+        # Add a button
+        start_btn = QPushButton(u'Создать ...')
+        start_btn.setToolTip(u'Нажмите, чтобы начать создавать листы')
+        start_btn.clicked.connect(start)
+        # btn.resize(btn.sizeHint())
+
+        # Add a button
+        exit_btn = QPushButton(u'Выход')
+        exit_btn.setToolTip(u'Нажмите, чтобы завершить работу')
+        exit_btn.clicked.connect(exit_f)
+        # btn.resize(btn.sizeHint())
+
+        # Add a button
+        about_btn = QPushButton(u'О программе ...')
+        # about_btn.setToolTip(u'Нажмите, чтобы составить листы')
+        about_btn.clicked.connect(about)
+        # btn.resize(btn.sizeHint())
+
+        # Приветствие
+        gr = QLabel(u'Это альфа-версия программы LoS creator\n'
+                    u'Возможны баги')
+
+        hbox.addWidget(start_btn)
+        hbox.addWidget(exit_btn)
+        hbox.addWidget(about_btn)
+        groupBox.setLayout(hbox)
+
+        # добавляем виджеты в грид
+        main_layout.addWidget(gr, 1, 1)
+        main_layout.addWidget(groupBox, 2, 1)
+
+        # завершаем создание окна и высвечиваем
+        self.setLayout(main_layout)
+        self.show()
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+
+class TwoListsWindow(QWidget):
+    def __init__(self):
+        super(TwoListsWindow, self).__init__()
         self.initUI()
 
     def initUI(self):
@@ -173,10 +246,17 @@ class Window(QWidget):
         groupBox2.setLayout(vbox)
 
         def go():
-            self.close()
-            # print arguments_list1.currentIndex()
-            # pass
-            # print cb.checkState(), button_group.checkedId()
+            # if cb_differ.checkState():
+                # self.close()
+            SetDiffer()
+                # , button_group.checkedId()
+
+        # def differ():
+        #     pass
+
+        # добавляем чек для разных листов
+        cb_differ = QCheckBox(u'Листы должны отличаться на один параметр')
+        # cb_differ.toggled.connect(differ)
 
         # Add a button
         btn = QPushButton(u'Далее >')
@@ -185,10 +265,84 @@ class Window(QWidget):
         # btn.resize(btn.sizeHint())
 
         # добавляем виджеты в грид
-        main_layout.addWidget(groupBox, 1, 1)
-        main_layout.addWidget(groupBox2, 1, 2)
+        main_layout.addWidget(groupBox, 1, 1, 1, 2)
+        main_layout.addWidget(groupBox2, 1, 3, 1, 2)
+        main_layout.addWidget(cb_differ, 2, 2, 1, 2)
         # main_layout.setColumnStretch(0, 2)
-        main_layout.addWidget(btn, 2, 1, 1, 2)
+        main_layout.addWidget(btn, 3, 2, 1, 2)
+
+        # завершаем создание окна и высвечиваем
+        self.setLayout(main_layout)
+        self.show()
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+
+class StartWindow(QWidget):
+    def __init__(self):
+        super(StartWindow, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.center()
+        self.setWindowTitle(u'LoS creator 0.1')
+
+        # создаем главный грид
+        main_layout = QGridLayout()
+
+        def start():
+            TwoListsWindow()
+            # print arguments_list1.currentIndex()
+            # print cb.checkState(), button_group.checkedId()
+
+        def exit_f():
+            self.close()
+
+        def about():
+            w = QWidget()
+            QMessageBox.about(w, u'About', u'LoS creator version 0.1\n'
+                                           u'author: gree-gorey\n'
+                                           u'repository: https://github.com/gree-gorey/listOfStimuli_creator')
+            self.show()
+
+        # создаем поле кнопок
+        groupBox = QGroupBox()
+        hbox = QHBoxLayout()
+
+        # Add a button
+        start_btn = QPushButton(u'Создать ...')
+        start_btn.setToolTip(u'Нажмите, чтобы начать создавать листы')
+        start_btn.clicked.connect(start)
+        # btn.resize(btn.sizeHint())
+
+        # Add a button
+        exit_btn = QPushButton(u'Выход')
+        exit_btn.setToolTip(u'Нажмите, чтобы завершить работу')
+        exit_btn.clicked.connect(exit_f)
+        # btn.resize(btn.sizeHint())
+
+        # Add a button
+        about_btn = QPushButton(u'О программе ...')
+        # about_btn.setToolTip(u'Нажмите, чтобы составить листы')
+        about_btn.clicked.connect(about)
+        # btn.resize(btn.sizeHint())
+
+        # Приветствие
+        gr = QLabel(u'Это альфа-версия программы LoS creator\n'
+                    u'Возможны баги')
+
+        hbox.addWidget(start_btn)
+        hbox.addWidget(exit_btn)
+        hbox.addWidget(about_btn)
+        groupBox.setLayout(hbox)
+
+        # добавляем виджеты в грид
+        main_layout.addWidget(gr, 1, 1)
+        main_layout.addWidget(groupBox, 2, 1)
 
         # завершаем создание окна и высвечиваем
         self.setLayout(main_layout)
@@ -203,7 +357,9 @@ class Window(QWidget):
 
 def main():
     app = QApplication(sys.argv)
-    w = Window()
+    # StartWindow()
+    # TwoListsWindow()
+    SetDiffer()
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
